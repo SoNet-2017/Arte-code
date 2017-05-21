93f8eb8d653ed5c1241afdc89449efdffd8b88ad
grimaldellobello <grimi.david@gmail.com>
2017-05-21 14:14:59 +0200

    Impl_Firebase2

--- app/app.js
+++ app/app.js
@@ -1,12 +1,22 @@
 'use strict';
 
+// Initialize the Firebase SDK
+var config = {
+    apiKey: "AIzaSyB038goORO0c8K4g3lsSk9ExrBPsB_6A_s",
+    authDomain: "arte-853a7.firebaseapp.com",
+    databaseURL: "https://arte-853a7.firebaseio.com",
+    projectId: "arte-853a7",
+    storageBucket: "arte-853a7.appspot.com"
+};
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
+    "firebase",
   'ngRoute',
   'myApp.homeView',
-  'myApp.view2',
   'myApp.loginView',
-  'myApp.version'
+    'myApp.eventView',
+    'myApp.evento',
+    'myApp.version'
 ])
     .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
