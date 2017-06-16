848c4122e40b4736839c7e2b5797328e4f5091dc
S234591 <grimaldellobello@git.it>
2017-06-16 14:28:58 +0200

    ng-repeat own-carousel

--- app/app.js
+++ app/app.js
@@ -16,7 +16,6 @@ angular.module('myApp', [
   'ngRoute',
     'ngMap',
     'ngMessages',
-    'ocNgRepeat',
     'myApp.calendar',
     'myApp.homeView',
     'myApp.eventView',
--- app/userProfile/userProfileView.js
+++ app/userProfile/userProfileView.js
@@ -28,19 +28,6 @@ angular.module('myApp.userProfileView', ['ngRoute'])
 
 
 
-        var ctrl;
-        ctrl = this;
-        ctrl.members = []
-
-        ctrl.carouselInitializer = function() {
-            $(".about-carousel").owlCarousel({
-                items: 3,
-                navigation: true,
-                pagination: false,
-                navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
-            });
-        };
-
 
 
 
